008435075a32528a40549e0801fd1ec0f9aa48c3
Manesh K <maneshk27@gmail.com>
2024-07-19 17:09:00 +0530

    Delete demo_app directory

--- demo_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sample_msg/', views.sample_msg, name='sample_msg'),
-    path('index/', views.new_page, name='indx'),
-]
